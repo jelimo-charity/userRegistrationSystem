4e09f00ba92a7365d6b25f1dc25b3813f7d60f58
jelimo-charity <charityjelimo893@gmail.com>
2023-05-15 04:44:24 +0300

    updates

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+let selectedRow = null;
 function submitData(){
     let formData = readFormData();
-    insertData(formData);
+    if( selectedRow == null ){
+        insertData(formData);
 
+    }else{
+        updateRecords(formData);
+
+    }
 }
+resetForm();
  function readFormData(){
     let formData = {};
     formData['username'] = document.getElementById('username').value;
@@ -26,7 +33,36 @@ function submitData(){
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.country; 
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<a>Update</a>
+    cell5.innerHTML = `<a onClick="updateBtn(this)">Update</a>
                         <a>Delete</a>`;
 
  }
+
+ //update data
+  function updateBtn(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('username').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('idnumber').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('language').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('country').value = selectedRow.cells[3].innerHTML;
+
+
+  }
+  function updateRecords(formData){
+    selectedRow.cells[0].innerHTML = formData.username;
+  selectedRow.cells[1].innerHTML = formData.idnumber;
+  selectedRow.cells[2].innerHTML = formData.language;
+  selectedRow.cells[3].innerHTML = formData.country;
+
+  }
+  //Delete data
+  function deleteData(td){
+    if( confirm( 'Are you sure you want to delete the user data?')){
+        rowDeleted = td.parentElement.parentElement;
+        document.getElementById('userList').deleteRow(rowDeleted.rowDeletedIndex);
+        resetForm(); 
+
+    }
+  
+}
+
